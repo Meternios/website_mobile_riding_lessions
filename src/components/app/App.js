e01cf68f7492988844109eecede0d17695d70ac8
Florian Hitz <web.florianhitz@gmail.com>
2019-08-31 00:50:32 +0200

    Material React Components

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,18 +4,23 @@ import Header from '../header/Header.js';
 import Footer from '../footer/Footer.js';
 import Main from '../main/Main.js';
 import { BrowserRouter as Router } from 'react-router-dom';
+import '../../assets/react-toolbox/theme.css';
+import theme from '../../assets/react-toolbox/theme.js';
+import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
 
 function App() {
   return (
-    <div className="fh-structure">
-      <Router>
-        <Header></Header>
+    <ThemeProvider theme={theme}>
+      <div className="fh-structure">
+        <Router>
+          <Header></Header>
 
-        <Main></Main>
-      
-        <Footer></Footer>
-      </Router>
-    </div>
+          <Main></Main>
+        
+          <Footer></Footer>
+        </Router>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Home.css';
+import Button from 'react-toolbox/lib/button/Button';
 
 function Home() {
     return (
         <div>
+            <Button label="Test Button" raised primary />
             <h1>home</h1>
         </div>
     );
